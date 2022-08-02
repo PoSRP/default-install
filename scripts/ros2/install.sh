f41b2d285853ab78ba59856b3ce35814db1bdd2e
sr <soerenriisom@gmail.com>
2022-08-02 23:12:56 +0200

    .

--- scripts/ros2/install.sh
+++ scripts/ros2/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apt-get update
+apt-get update -q
 apt-get install -qy locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
@@ -15,7 +15,6 @@ echo "deb [arch=$(dpkg --print-architecture) \
   tee /etc/apt/sources.list.d/ros2.list &> /dev/null
 
 apt-get update -q
-apt-get upgrade -qy
 apt-get install -qy ros-humble-desktop
 apt-get install -qy python3-colcon-ros python3-colcon-bash python3-colcon-zsh python3-rosdep
 
